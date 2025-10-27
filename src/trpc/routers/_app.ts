fc54fac4035c6458c6d9168381fcaacfef0f86a0
renatomarf76 <renatomarf76@egmail.com>
2025-10-27 19:02:51 -0300

    10: Payments Setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,17 +1,12 @@
 import { inngest } from '@/inngest/client';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { createTRPCRouter, premiumProcedure, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { google } from '@ai-sdk/google';
 import { generateText } from 'ai';
 import { TRPCError } from '@trpc/server';
 
 export const appRouter = createTRPCRouter({
-  testAi: baseProcedure.mutation(async () => {
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Something went wrong"
-    });
-    
+  testAi: premiumProcedure.mutation(async () => {
     await inngest.send({
       name: "execute/ai",
     });
