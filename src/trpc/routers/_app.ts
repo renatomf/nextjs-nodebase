bd8de16413fcd6cb27bc7b2b4ecc2c0dc9d4a05d
renatomarf76 <renatomarf76@egmail.com>
2025-10-27 15:31:55 -0300

    08: Error Tracking

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,9 +3,15 @@ import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { google } from '@ai-sdk/google';
 import { generateText } from 'ai';
+import { TRPCError } from '@trpc/server';
 
 export const appRouter = createTRPCRouter({
   testAi: baseProcedure.mutation(async () => {
+    throw new TRPCError({
+      code: "BAD_REQUEST",
+      message: "Something went wrong"
+    });
+    
     await inngest.send({
       name: "execute/ai",
     });
